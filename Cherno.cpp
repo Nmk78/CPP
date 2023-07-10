88380749ca41a8569c0f9a1e1d19ffaf671787cc
Nmk78 <naymyokhant78@gmail.com>
2023-07-10 20:23:07 +0630

    Add project files.

---
+++ Cherno.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Logger.h"
+
+using namespace std;
+
+int main()
+{
+    void subFile(int* num);
+    void log(const char* message);
+ /*   for (int i = 0; i <= 10; i++)
+    {
+        int a = i;
+        cout << a << endl;
+       const char* b = "Hello";
+       cout << b << endl;
+    }
+    */
+    const int z = 0;
+    if(z = 0)
+        log("Hello World");
+
+
+    return 0;
+}
---
+++ Logger.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+void log(const char* message)
+{
+	std::cout << message << std::endl;
+}
+
+void subFile(int* num)
+{
+	std::cout << num << std::endl;
+}
